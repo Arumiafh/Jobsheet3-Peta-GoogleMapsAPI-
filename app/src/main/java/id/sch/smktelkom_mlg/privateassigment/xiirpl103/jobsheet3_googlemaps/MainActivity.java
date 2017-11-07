f3e0f4a4365d21fac136ed2a4d753d042a652902
arumiafh <arumiafh@gmail.com>
2017-11-08 00:56:25 +0700

    Custom Marker

--- app/src/main/java/id/sch/smktelkom_mlg/privateassigment/xiirpl103/jobsheet3_googlemaps/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/privateassigment/xiirpl103/jobsheet3_googlemaps/MainActivity.java
@@ -15,22 +15,29 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
-    static final CameraPosition JAKARTA = CameraPosition.builder()
+    static final CameraPosition INDONESIA = CameraPosition.builder()
             .target(new LatLng(-6.21462, 106.84513))
-            .zoom(5)
+            .zoom(2)
+            .bearing(0)
+            .tilt(45)
+            .build();
+    static final CameraPosition AUSTRALIA = CameraPosition.builder()
+            .target(new LatLng(-33.856820, 151.215279))
+            .zoom(2)
+            .bearing(0)
+            .tilt(45)
+            .build();
+    static final CameraPosition MALAYSIA = CameraPosition.builder()
+            .target(new LatLng(3.1412, 101.68653))
+            .zoom(2)
             .bearing(0)
             .tilt(45)
             .build();
 
     GoogleMap m_map;
     boolean mapReady = false;
-    MarkerOptions bandung;
-    MarkerOptions makassar;
-    MarkerOptions malang;
-    MarkerOptions bali;
-    MarkerOptions manado;
-    MarkerOptions aceh;
-    MarkerOptions palu;
+    MarkerOptions aust;
+    MarkerOptions malay;
 
     @Override
     public Resources getResources() {
@@ -42,40 +49,16 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        bandung = new MarkerOptions()
-                .position(new LatLng(-6.90389, 107.61861))
-                .title("Bandung")
+        aust = new MarkerOptions()
+                .position(new LatLng(-33.856820, 151.215279))
+                .title("Sydney")
                 .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher));
 
-        makassar = new MarkerOptions()
-                .position(new LatLng(-5.14, 119.4221))
-                .title("Makassar")
+        malay = new MarkerOptions()
+                .position(new LatLng(3.1412, 101.68653))
+                .title("Kuala Lumpur")
                 .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher));
 
-        malang = new MarkerOptions()
-                .position(new LatLng(-7.9797, 112.6304))
-                .title("Malang")
-                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher));
-
-        bali = new MarkerOptions()
-                .position(new LatLng(-8.65, 115.21667))
-                .title("Denpasar")
-                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher));
-
-        manado = new MarkerOptions()
-                .position(new LatLng(1.48218, 124.84892))
-                .title("Manado")
-                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher));
-
-        aceh = new MarkerOptions()
-                .position(new LatLng(5.5577, 95.3222))
-                .title("Aceh")
-                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher));
-
-        palu = new MarkerOptions()
-                .position(new LatLng(-0.8917, 119.8707))
-                .title("Palu")
-                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher));
 
         MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
@@ -85,14 +68,9 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     public void onMapReady(GoogleMap map) {
         mapReady = true;
         m_map = map;
-        m_map.addMarker(bandung);
-        m_map.addMarker(makassar);
-        m_map.addMarker(malang);
-        m_map.addMarker(bali);
-        m_map.addMarker(manado);
-        m_map.addMarker(aceh);
-        m_map.addMarker(palu);
-        flyTo(JAKARTA);
+        m_map.addMarker(aust);
+        m_map.addMarker(malay);
+        flyTo(INDONESIA);
     }
 
     private void flyTo(CameraPosition target) {
