7759d0c1e29095edb23ecb03d5b5fd9881d01690
arumiafh <arumiafh@gmail.com>
2017-11-08 01:27:40 +0700

    StreetView

--- app/src/main/java/id/sch/smktelkom_mlg/privateassigment/xiirpl103/jobsheet3_googlemaps/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/privateassigment/xiirpl103/jobsheet3_googlemaps/MainActivity.java
@@ -1,63 +1,32 @@
 package id.sch.smktelkom_mlg.privateassigment.xiirpl103.jobsheet3_googlemaps;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.CameraPosition;
-import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.OnStreetViewPanoramaReadyCallback;
+import com.google.android.gms.maps.StreetViewPanorama;
+import com.google.android.gms.maps.StreetViewPanoramaFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.StreetViewPanoramaCamera;
 
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
-
-    static final CameraPosition INDONESIA = CameraPosition.builder()
-            .target(new LatLng(-6.175392, 106.827178))
-            .zoom(15)
-            .bearing(0)
-            .tilt(45)
-            .build();
-
-    GoogleMap m_map;
-    boolean mapReady = false;
-
-    LatLng IND = new LatLng(-6.175392, 106.827178);
-    MarkerOptions Indonesia;
+public class MainActivity extends AppCompatActivity implements OnStreetViewPanoramaReadyCallback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Indonesia = new MarkerOptions()
-                .position(new LatLng(-6.175392, 106.827178))
-                .title("Monumen Nasional")
-                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher));
-
-        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
-    }
-
-    private void flyTo(CameraPosition target) {
-        m_map.animateCamera(CameraUpdateFactory.newCameraPosition(target), 10000, null);
+        StreetViewPanoramaFragment streetViewPanoramaFragment = (StreetViewPanoramaFragment) getFragmentManager().findFragmentById(R.id.streetviewpanorama);
+        streetViewPanoramaFragment.getStreetViewPanoramaAsync(this);
     }
 
     @Override
-    public void onMapReady(GoogleMap map) {
-        mapReady = true;
-        m_map = map;
-        map.moveCamera(CameraUpdateFactory.newCameraPosition(INDONESIA));
-        map.addCircle(new CircleOptions()
-                .center(IND)
-                .radius(500)
-                .strokeColor(Color.GREEN)
-                .fillColor(Color.argb(64, 0, 255, 0)));
-        m_map.addMarker(Indonesia);
+    public void onStreetViewPanoramaReady(StreetViewPanorama panorama) {
+        panorama.setPosition(new LatLng(-7.973581, 112.621113));
+        StreetViewPanoramaCamera camera = new StreetViewPanoramaCamera.Builder()
+                .bearing(0)
+                .build();
+        panorama.animateTo(camera, 10000);
     }
 
 }
